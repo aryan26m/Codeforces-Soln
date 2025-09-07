b3a2601ffc50ec7c29e387d9e04b0ce4f742250d
aryan26aa <shivaverma7777@gmail.com>
2025-09-07 10:40:25 +0530

    cses shortest route

--- B_Count_Subrectangles.cpp
+++ B_Count_Subrectangles.cpp
@@ -1,152 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define debug(x) cerr << #x << 
-#define int long long
-#define pb push_back
-#define all(x) (x).begin(), (x).end()
-#define vi vector<int>
-#define pii pair<int,int>
-#define rep(i, a, b) for(int i = a; i < b; ++i)
-#define fast ios::sync_with_stdio(false); cin.tie(0);
-const int MOD = 1e9 + 7;
-const int INF = 1e18;
-
-// Input array
-vi enterv(int n) {
-    vi a(n);
-    for (int &x : a) cin >> x;
-    return a;
-}
-
-// Debug print
-void printArray(const vi &a) {
-    for (int x : a) cout << x << ' ';
-    cout << '\n';
-}
-
-// GCD
-int gcd(int a, int b) {
-    if (b == 0) return a;
-    return gcd(b, a % b);
-}
-
-// Prime check
-bool isPrime(int n) {
-    if (n < 2) return false;
-    if (n == 2 || n == 3) return true;
-    if (n % 2 == 0 || n % 3 == 0) return false;
-    for (int i = 5; i * i <= n; i += 6) {
-        if (n % i == 0 || n % (i + 2) == 0) return false;
-    }
-    return true;
-}
-
-// Binary Search
-bool binarySearch(const vi &a, int target) {
-    int low = 0, high = a.size() - 1;
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        if (a[mid] == target) return true;
-        if (a[mid] < target) low = mid + 1;
-        else high = mid - 1;
-    }
-    return false;
-}
-
-// nCr using Fermat's Little Theorem
-int power(int x, int y, int p) {
-    int res = 1;
-    x %= p;
-    while (y > 0) {
-        if (y & 1) res = res * x % p;
-        y >>= 1;
-        x = x * x % p;
-    }
-    return res;
-}
-
-int modInverse(int n, int p) {
-    return power(n, p - 2, p);
-}
-
-int nCr(int n, int r, int p = MOD) {
-    if (r > n || r < 0) return 0;
-    static vector<int> fact(1, 1);
-    while ((int)fact.size() <= n)
-        fact.push_back(fact.back() * fact.size() % p);
-    return fact[n] * modInverse(fact[r], p) % p * modInverse(fact[n - r], p) % p;
-}
-
-// Comparator (Descending Order)
-bool comp(int a, int b) {
-    return a > b;
-}
-
-// Frequency Map Update
-void push(map<int, int> &mp, int k, int v) {
-    mp[k] += v;
-}
-
-// Solve Function
-void solve() {
-    // Write your logic here
-    int n,m,k;
-    cin>>n>>m>>k;
-    vi a=enterv(n);
-    vi b=enterv(m);
-    vector<int> v1(n+1);
-    vector<int> v2(m+1);
-    int cnt=0;
-
-    for(int i=0;i<n;i++){
-             if(a[i]==1){
-                cnt++;
-                v1[cnt]+=1;
-             }
-             else{
-                cnt=0;
-             }
+class Solution {
+    vector<int> nextGreaterRight(const vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n, n);
+        stack<int> st;
+        for (int i = n-1; i >= 0; i--) {
+            while (!st.empty() && nums[st.top()] < nums[i]) st.pop();
+            if (!st.empty()) res[i] = st.top();
+            st.push(i);
+        }
+        return res;
     }
 
-    cnt=0;
-    
-    for(int i=0;i<m;i++){
-             if(b[i]==1){
-                cnt++;
-                v2[cnt]+=1;
-             }
-             else{
-                cnt=0;
-             }
-    }
+public:
+    long long bowlSubarrays(vector<int>& parvostine) {
+        int n = parvostine.size();
+        vector<int> right = nextGreaterRight(parvostine);
 
-    for(int i=m-1;i>=1;i--){
-            v2[i]=v2[i+1]+v2[i];
-    }
-    for(int i=n-1;i>=1;i--){
-            v1[i]=v1[i+1]+v1[i];
-    }
-
-    // printArray(v2);
-    int ans=0;
-    for(int i=1;i*i<=k;i++){
-        if((k%i)==0){
-            int y=k/i;
-            if(i<=n && y<=m){
-                ans+=(v1[i]*v2[y]);
-            }
-            if(i!=y && y<=n && i<=m){
-                ans+=(v1[y]*v2[i]);
+        long long ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (right[i] < n) {
+                ans += max(0, right[i] - i - 1); // count only from left wall
             }
         }
+        return ans;
     }
+};
 
-    cout<<ans<<endl;
+int main() {
+    Solution sol;
 
+    vector<int> nums = {5,1,2,3,4};
+    cout << sol.bowlSubarrays(nums) << "\n"; // Expected: 3
 }
 
-int32_t main() {
-    fast; solve();
-    return 0;
-}
+
--- B_Vicious_Labyrinth.cpp
+++ B_Vicious_Labyrinth.cpp
@@ -1,42 +1,65 @@
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
+// Count bowl subarrays:
+// A subarray [l, r] (r - l + 1 >= 3) is a bowl if
+// min(nums[l], nums[r]) > max(nums[l+1..r-1]).
+//
+// Key observation with distinct elements:
+// For any bowl [l, r], the interior maximum occurs at some unique index p (l < p < r).
+// Then l must be the nearest greater to the left of p, and r must be the nearest greater to the right of p.
+// Therefore there is a 1-1 correspondence between bowls and indices p that have both a
+// nearest greater on the left and a nearest greater on the right.
+// So, the answer is simply the count of indices p with NGL[p] != -1 and NGR[p] != n.
+
 class Solution {
-public:
-    long long maxProduct(vector<int>& nums) {
-        
-        for(auto &x:nums){
-            maxo = max(maxo,x);
-        }
-        int mxbit = 0;
-        while ((1 << mxbit) <= maxo){
-            mxbit++;
-        } 
-        int N = (1 << mxbit);
-
-        vector<int> maxi(N, 0);
-        for (int x : nums) {
-            maxi[x] = max(maxi[x], x);
-        }
-
-        for (int i = 0; i < mxbit; ++i)
-         {
-            for (int mask = 0; mask < N; ++mask) 
-            {
-                if (mask & (1 << i)) 
-                {
-                    maxi[mask] = max(maxi[mask], maxi[mask ^ (1 << i)]);
-                }
-            }
-        }
-
-        long long res = 0;
-        for (int x : nums) {
-            int inv = ((1 << mxbit) - 1) ^ x; 
-
-            res = max(res, 1LL * x * maxi[inv]);
+    vector<int> nextGreaterRight(const vector<int>& a) {
+        int n = (int)a.size();
+        vector<int> res(n, n);
+        stack<int> st; // decreasing stack by value (indices)
+        for (int i = n - 1; i >= 0; --i) {
+            while (!st.empty() && a[st.top()] < a[i]) st.pop();
+            if (!st.empty()) res[i] = st.top();
+            st.push(i);
         }
         return res;
     }
+
+    vector<int> nextGreaterLeft(const vector<int>& a) {
+        int n = (int)a.size();
+        vector<int> res(n, -1);
+        stack<int> st; // decreasing stack by value (indices)
+        for (int i = 0; i < n; ++i) {
+            while (!st.empty() && a[st.top()] < a[i]) st.pop();
+            if (!st.empty()) res[i] = st.top();
+            st.push(i);
+        }
+        return res;
+    }
+
+public:
+    long long bowlSubarrays(vector<int>& nums) {
+        vector<int> v = nums;
+
+        int n = (int)v.size();
+        if (n < 3) return 0;
+
+        vector<int> L = nextGreaterLeft(v);
+        vector<int> R = nextGreaterRight(v);
+
+        long long ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (L[i] != -1 && R[i] != n) {
+                ans += 1;
+            }
+        }
+        return ans;
+    }
 };
+
+int main() {
+    Solution sol;
+    vector<int> nums = {5, 1, 2, 3, 4};
+    cout << sol.bowlSubarrays(nums) << "\n"; // Expected: 3
+    return 0;
+}
