9ea47f7eb3fb22eaed9e989084098d8741d0a72f
aryan26aa <shivaverma7777@gmail.com>
2026-01-07 15:38:30 +0530

    bits

--- tuf.cpp
+++ tuf.cpp
@@ -3,19 +3,30 @@ public:
     int singleNumber(vector<int>& nums) {        
         //your code goes here 
         int n=nums.size();
-        int ans=0;
-        for(int i=0;i<32;i++){
-            int sum=0;
-            for(int j=0;j<n;j++){
-                if ((nums[j]>>i)&1)
-                {
-                    sum++;
-                }
+        int ans[2];
+        int x=0;
+        for(int i=0;i<n;i++){
+            x=x^nums[i];
+        }
+        int right=(x&(x-1))^x;
+        int ans1=0;
+        int ans2=0;
+        for(int i=0;i<n;i++)
+        {
+            if((nums[i]&right)){
+                ans1^=nums[i];
             }
-            if(sum%3){
-
-                ans|=(1<<i);
+            else{
+                   ans2^=nums[i];
             }
+        } 
+    
+        if(ans1<ans2){
+            ans[0]=ans1;
+            ans[1]=ans2;
+        }else{
+               ans[1]=ans1;
+            ans[0]=ans2;
         }
         return ans;
     }
